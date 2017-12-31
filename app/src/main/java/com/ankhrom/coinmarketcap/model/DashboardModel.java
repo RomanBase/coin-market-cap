b068f82e69725ea142154f955475995a42e1a22d
RomanBase <ro.hornak@gmail.com>
2017-12-31 12:23:53 +0100

    timestamp

--- app/src/main/java/com/ankhrom/coinmarketcap/model/DashboardModel.java
+++ app/src/main/java/com/ankhrom/coinmarketcap/model/DashboardModel.java
@@ -8,6 +8,7 @@ import com.ankhrom.coinmarketcap.api.ApiMath;
 import com.ankhrom.coinmarketcap.api.MarketData;
 
 import java.util.Collection;
+import java.util.Date;
 
 /**
  * Created by R' on 12/30/2017.
@@ -17,12 +18,14 @@ public class DashboardModel extends AdapterModel<CoinItemModel> {
 
     public final String marketCap;
     public final String marketVolume;
+    public final String date;
 
     public DashboardModel(Context context, Collection<CoinItemModel> collection, MarketData market) {
         super(context, collection);
 
         marketCap = ApiMath.toShortFormat(String.valueOf(market.marketCap));
         marketVolume = ApiMath.toShortFormat(String.valueOf(market.marketVolume));
+        date = new Date(market.timestamp * 1000).toLocaleString();
     }
 
     @Override
