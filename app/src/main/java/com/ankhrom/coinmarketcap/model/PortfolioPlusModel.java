5e7c816cf395693bc9abe5e45ab4411df5433a07
RomanBase <ro.hornak@gmail.com>
2018-01-07 21:25:56 +0100

    btc units re-calc

--- app/src/main/java/com/ankhrom/coinmarketcap/model/PortfolioPlusModel.java
+++ app/src/main/java/com/ankhrom/coinmarketcap/model/PortfolioPlusModel.java
@@ -2,8 +2,6 @@ package com.ankhrom.coinmarketcap.model;
 
 import android.databinding.ObservableBoolean;
 
-import com.ankhrom.base.common.statics.StringHelper;
-import com.ankhrom.base.interfaces.OnValueChangedListener;
 import com.ankhrom.base.model.Model;
 import com.ankhrom.base.observable.EditTextObservable;
 import com.ankhrom.base.observable.ObservableString;
@@ -19,77 +17,10 @@ public class PortfolioPlusModel extends Model {
     public final EditTextObservable units = new EditTextObservable();
     public final EditTextObservable unitPrice = new EditTextObservable();
     public final EditTextObservable sumPrice = new EditTextObservable();
+    public final EditTextObservable bitcoinUnits = new EditTextObservable();
+    public final EditTextObservable bitcoinUnitValue = new EditTextObservable();
 
     public final ObservableBoolean editableCurrency = new ObservableBoolean(true);
-
-    public PortfolioPlusModel() {
-
-        units.setOnValueChangedListener(onUnitCountChanged);
-        unitPrice.setOnValueChangedListener(onUnitPriceChanged);
-        sumPrice.setOnValueChangedListener(onSumPriceChanged);
-    }
-
-    public final OnValueChangedListener<String> onUnitCountChanged = new OnValueChangedListener<String>() {
-        @Override
-        public void onValueChanged(String value) {
-
-            if (StringHelper.isEmpty(value)) {
-                return;
-            }
-
-            value = ensureFormat(value);
-
-            boolean sumAvailable = !StringHelper.isEmpty(sumPrice.get());
-            boolean unitAvailable = !StringHelper.isEmpty(unitPrice.get());
-
-            if (sumAvailable) {
-                unitPrice.setValue(String.valueOf(Double.parseDouble(sumPrice.get()) / Double.parseDouble(value)));
-            } else if (unitAvailable) {
-                sumPrice.setValue(String.valueOf(Double.parseDouble(unitPrice.get()) * Double.parseDouble(value)));
-            }
-        }
-    };
-
-    public final OnValueChangedListener<String> onUnitPriceChanged = new OnValueChangedListener<String>() {
-        @Override
-        public void onValueChanged(String value) {
-
-            if (StringHelper.isEmpty(value) || StringHelper.isEmpty(units.get())) {
-                sumPrice.setValue(null);
-                return;
-            }
-
-            value = ensureFormat(value);
-
-            double price = Double.parseDouble(value);
-            double amount = Double.parseDouble(units.get());
-
-            sumPrice.setValue(String.valueOf(price * amount));
-        }
-    };
-
-    public final OnValueChangedListener<String> onSumPriceChanged = new OnValueChangedListener<String>() {
-        @Override
-        public void onValueChanged(String value) {
-
-            if (StringHelper.isEmpty(value) || StringHelper.isEmpty(units.get())) {
-                unitPrice.setValue(null);
-                return;
-            }
-
-            value = ensureFormat(value);
-
-            double price = Double.parseDouble(value);
-            double amount = Double.parseDouble(units.get());
-
-            unitPrice.setValue(String.valueOf(price / amount));
-        }
-    };
-
-    private String ensureFormat(String value) {
-
-        return value.startsWith(".") || value.startsWith(",") ? value + "0" : value;
-    }
 
     @Override
     public int getVariableBindingResource() {
--- app/src/main/java/com/ankhrom/coinmarketcap/viewmodel/portfolio/PortfolioPlusViewModel.java
+++ app/src/main/java/com/ankhrom/coinmarketcap/viewmodel/portfolio/PortfolioPlusViewModel.java
@@ -4,7 +4,9 @@ import android.view.View;
 
 import com.ankhrom.base.common.statics.FragmentHelper;
 import com.ankhrom.base.common.statics.ScreenHelper;
+import com.ankhrom.base.common.statics.StringHelper;
 import com.ankhrom.base.custom.args.InitArgs;
+import com.ankhrom.base.interfaces.OnValueChangedListener;
 import com.ankhrom.coinmarketcap.R;
 import com.ankhrom.coinmarketcap.databinding.PortfolioPlusPageBinding;
 import com.ankhrom.coinmarketcap.entity.CoinItem;
@@ -23,6 +25,7 @@ import com.ankhrom.coinmarketcap.viewmodel.dialog.SearchViewModel;
 public class PortfolioPlusViewModel extends AppViewModel<PortfolioPlusPageBinding, PortfolioPlusModel> implements OnCoinSelectedListener {
 
     private CoinItem coin;
+    private CoinItem bitcoin;
     private OnPortfolioChangedListener listener;
 
     @Override
@@ -37,15 +40,135 @@ public class PortfolioPlusViewModel extends AppViewModel<PortfolioPlusPageBindin
     public void onInit() {
         super.onInit();
 
+
+    }
+
+    @Override
+    public void loadModel() {
+        super.loadModel();
+
         setModel(new PortfolioPlusModel());
+
+        bitcoin = getDataHolder().getCoin("bitcoin");
+
+        model.units.setOnValueChangedListener(onUnitCountChanged);
+        model.unitPrice.setOnValueChangedListener(onUnitPriceChanged);
+        model.sumPrice.setOnValueChangedListener(onSumPriceChanged);
+        model.bitcoinUnits.setOnValueChangedListener(onBitcoinUnitsChanged);
 
         if (coin != null) {
             model.editableCurrency.set(false);
             model.currency.set(coin.symbol + " - " + coin.name);
         } else {
-            coin = getDataHolder().getCoin("bitcoin");
+            coin = bitcoin;
             model.currency.set(coin.symbol + " - " + coin.name);
         }
+    }
+
+    public final OnValueChangedListener<String> onUnitCountChanged = new OnValueChangedListener<String>() {
+        @Override
+        public void onValueChanged(String value) {
+
+            if (StringHelper.isEmpty(value)) {
+                return;
+            }
+
+            value = ensureFormat(value);
+
+            boolean sumAvailable = !StringHelper.isEmpty(model.sumPrice.get());
+            boolean unitAvailable = !StringHelper.isEmpty(model.unitPrice.get());
+            double btcPrice = Double.parseDouble(bitcoin.priceUsd);
+
+            if (sumAvailable) {
+                double price = Double.parseDouble(model.sumPrice.get());
+                double unitPrice = price / Double.parseDouble(value);
+                model.unitPrice.setValue(String.valueOf(unitPrice));
+                model.bitcoinUnits.setValue(String.valueOf(price / btcPrice));
+                model.bitcoinUnitValue.setValue(String.valueOf(unitPrice / btcPrice));
+            } else if (unitAvailable) {
+                double unitPrice = Double.parseDouble(model.unitPrice.get());
+                double price = unitPrice * Double.parseDouble(value);
+                model.sumPrice.setValue(String.valueOf(price));
+                model.bitcoinUnits.setValue(String.valueOf(price / btcPrice));
+                model.bitcoinUnitValue.setValue(String.valueOf(unitPrice / btcPrice));
+            }
+        }
+    };
+
+    public final OnValueChangedListener<String> onUnitPriceChanged = new OnValueChangedListener<String>() {
+        @Override
+        public void onValueChanged(String value) {
+
+            if (StringHelper.isEmpty(value) || StringHelper.isEmpty(model.units.get())) {
+                model.sumPrice.setValue(null);
+                model.bitcoinUnits.setValue(null);
+                model.bitcoinUnitValue.setValue(null);
+                return;
+            }
+
+            value = ensureFormat(value);
+
+            double unitPrice = Double.parseDouble(value);
+            double amount = Double.parseDouble(model.units.get());
+            double price = unitPrice * amount;
+            double btcPrice = Double.parseDouble(bitcoin.priceUsd);
+
+            model.sumPrice.setValue(String.valueOf(price));
+            model.bitcoinUnits.setValue(String.valueOf(price / btcPrice));
+            model.bitcoinUnitValue.setValue(String.valueOf(unitPrice / btcPrice));
+        }
+    };
+
+    public final OnValueChangedListener<String> onSumPriceChanged = new OnValueChangedListener<String>() {
+        @Override
+        public void onValueChanged(String value) {
+
+            if (StringHelper.isEmpty(value) || StringHelper.isEmpty(model.units.get())) {
+                model.unitPrice.setValue(null);
+                model.bitcoinUnits.setValue(null);
+                model.bitcoinUnitValue.setValue(null);
+                return;
+            }
+
+            value = ensureFormat(value);
+
+            double price = Double.parseDouble(value);
+            double amount = Double.parseDouble(model.units.get());
+            double unitPrice = price / amount;
+            double btcPrice = Double.parseDouble(bitcoin.priceUsd);
+
+            model.unitPrice.setValue(String.valueOf(unitPrice));
+            model.bitcoinUnits.setValue(String.valueOf(price / btcPrice));
+            model.bitcoinUnitValue.setValue(String.valueOf(unitPrice / btcPrice));
+        }
+    };
+
+    public final OnValueChangedListener<String> onBitcoinUnitsChanged = new OnValueChangedListener<String>() {
+        @Override
+        public void onValueChanged(String value) {
+
+            if (StringHelper.isEmpty(value) || StringHelper.isEmpty(model.units.get())) {
+                return;
+            }
+
+            value = ensureFormat(value);
+
+            double btcUnits = Double.parseDouble(value);
+            double amount = Double.parseDouble(model.units.get());
+            double btcPrice = Double.parseDouble(bitcoin.priceUsd);
+
+            double price = btcUnits * btcPrice;
+            double unitPrice = price / amount;
+
+            model.sumPrice.setValue(String.valueOf(price));
+            model.unitPrice.setValue(String.valueOf(unitPrice));
+            model.bitcoinUnitValue.setValue(String.valueOf(unitPrice / btcPrice));
+        }
+    };
+
+    private String ensureFormat(String value) {
+
+        return value.startsWith(".") || value.startsWith(",") ? value + "0" : value;
     }
 
     public void onSearchPressed(View view) {
