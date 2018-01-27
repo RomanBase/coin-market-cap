469a15a92f03b2f25beadecadc4675044b1be93b
RomanBase <ro.hornak@gmail.com>
2018-01-27 22:56:25 +0100

    firebase db

---
+++ app/src/main/java/com/ankhrom/coinmarketcap/entity/FeatureRequest.java
@@ -0,0 +1,14 @@
+package com.ankhrom.coinmarketcap.entity;
+
+/**
+ * Created by R' on 1/27/2018.
+ */
+
+public class FeatureRequest {
+
+    public String name;
+    public String mail;
+    public String address;
+    public String message;
+    public long timestamp;
+}
--- app/src/main/java/com/ankhrom/coinmarketcap/viewmodel/dialog/FeatureRequestViewModel.java
+++ app/src/main/java/com/ankhrom/coinmarketcap/viewmodel/dialog/FeatureRequestViewModel.java
@@ -4,11 +4,15 @@ import android.view.View;
 
 import com.ankhrom.base.common.statics.FragmentHelper;
 import com.ankhrom.base.common.statics.ScreenHelper;
+import com.ankhrom.base.common.statics.StringHelper;
+import com.ankhrom.base.custom.builder.ToastBuilder;
 import com.ankhrom.coinmarketcap.R;
 import com.ankhrom.coinmarketcap.databinding.FeatureRequestPageBinding;
+import com.ankhrom.coinmarketcap.entity.FeatureRequest;
 import com.ankhrom.coinmarketcap.listener.OnQRHandledListener;
 import com.ankhrom.coinmarketcap.model.dialog.FeatureRequestModel;
 import com.ankhrom.coinmarketcap.viewmodel.base.AppViewModel;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by R' on 1/27/2018.
@@ -28,6 +32,26 @@ public class FeatureRequestViewModel extends AppViewModel<FeatureRequestPageBind
     }
 
     public void onSendPressed(View view) {
+
+        if (StringHelper.isEmpty(model.message.get())) {
+            return;
+        }
+
+        FeatureRequest data = new FeatureRequest();
+        data.name = model.name.get();
+        data.mail = model.mail.get();
+        data.address = model.donation.get();
+        data.message = model.message.get();
+        data.timestamp = System.currentTimeMillis();
+
+        FirebaseDatabase.getInstance()
+                .getReference("feature_requests")
+                .push()
+                .setValue(data);
+
+        ToastBuilder.with(getContext())
+                .text("We received you message. Thanks.")
+                .buildAndShow();
 
         close();
     }
